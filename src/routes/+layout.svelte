b7548acd0515566e39221bbb2a94044da51fe3eb
dvdzhou <undavidzhou@gmail.com>
2026-04-28 23:21:51 +0200

    Import font and setup tokens folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,17 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+    // Import custom tokens
+    import "$lib/styles/tokens.css";
+
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+    <link rel="icon" href={favicon} />
+
+    <!-- Font Satoshi -->
+    <link rel="preconnect" href="https://api.fontshare.com" />
+    <link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet" />
+</svelte:head>
 {@render children()}
