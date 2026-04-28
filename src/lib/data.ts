b2e271845864810a43f1d1fa41c3c9c648f38436
dvdzhou <undavidzhou@gmail.com>
2026-04-29 00:31:07 +0200

    Import grid data and design tokens

---
+++ src/lib/data.ts
@@ -0,0 +1,74 @@
+export const cards = [
+  {
+    title: "Il primo respiro",
+    level: "Principiante",
+    duration: "5 min",
+    image: "/assets/card-1.jpg"
+  },
+  {
+    title: "Sentire il corpo",
+    level: "Principiante",
+    duration: "8 min",
+    image: "/assets/card-2.jpg"
+  },
+  {
+    title: "Una pausa, adesso",
+    level: "Principiante",
+    duration: "5 min",
+    image: "/assets/card-3.jpg"
+  },
+  {
+    title: "Lascia andare la giornata",
+    level: "Principiante",
+    duration: "10 min",
+    image: "/assets/card-4.jpg"
+  },
+  {
+    title: "Box breathing",
+    level: "Intermedio",
+    duration: "8 min",
+    image: "/assets/card-5.jpg"
+  },
+  {
+    title: "Body scan",
+    level: "Intermedio",
+    duration: "15 min",
+    image: "/assets/card-6.jpg"
+  },
+  {
+    title: "Camminata consapevole",
+    level: "Intermedio",
+    duration: "12 min",
+    image: "/assets/card-7.jpg"
+  },
+  {
+    title: "Non sei i tuoi pensieri",
+    level: "Intermedio",
+    duration: "10 min",
+    image: "/assets/card-8.jpg"
+  },
+  {
+    title: "Respirazione 4-7-8",
+    level: "Avanzato",
+    duration: "10 min",
+    image: "/assets/card-9.jpg"
+  },
+  {
+    title: "Vipassana",
+    level: "Avanzato",
+    duration: "20 min",
+    image: "/assets/card-10.jpg"
+  },
+  {
+    title: "Radicamento",
+    level: "Avanzato",
+    duration: "15 min",
+    image: "/assets/card-11.jpg"
+  },
+  {
+    title: "Il grande silenzio",
+    level: "Avanzato",
+    duration: "25 min",
+    image: "/assets/card-12.jpg"
+  }
+];
